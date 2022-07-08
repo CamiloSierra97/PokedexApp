56d4d20357aa64d3fb078e1df697b519a0ba3082
CamiloSierra97 <sierracamilo3@gmail.com>
2022-07-08 16:43:57 -0500

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 //Redux
-import { Provider } from 'react-redux'
 import store from './store'
 import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
